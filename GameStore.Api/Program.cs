f8b94478107e19e2a14e93cef8165aa0ff40215d
Ratko Mitrasinovic <ratko.mitrasinovic@combinedratio.com>
2026-02-09 22:53:34 +0100

    new methods added

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,6 @@
 using GameStore.Api.Dtos;
-using System.Collections.Generic;
 
+const string GetGameEndPointName = "GetGame";
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -22,9 +22,45 @@ List<GameDto> games = new List<GameDto>
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id) is GameDto game ? Results.Ok(game) : Results.NotFound());
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id) is GameDto game ? Results.Ok(game) : Results.NotFound())
+    .WithName(GetGameEndPointName);
 
 // POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+});
+
+// PUT /games/1
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+    return Results.NoContent();
+});
 
 
 app.Run();
